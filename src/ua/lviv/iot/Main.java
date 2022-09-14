c9d2538fc083aa87e405cd86d8b58803ea832e9b
moroznastya <nastya.moroz2210@gmail.com>
2022-09-14 13:10:33 +0300

    added code for lab1

---
+++ src/ua/lviv/iot/Main.java
@@ -0,0 +1,19 @@
+package ua.lviv.iot;
+
+public class Main {
+
+    public static void main(String[] args) {
+        RedBlackTree tree = new RedBlackTree();
+        tree.insert(55);
+        tree.insert(40);
+        tree.insert(65);
+        tree.insert(60);
+        tree.insert(75);
+        tree.insert(57);
+        tree.printTree();
+
+        System.out.println("\nAfter deleting:");
+        tree.deleteNode(40);
+        tree.printTree();
+    }
+}
