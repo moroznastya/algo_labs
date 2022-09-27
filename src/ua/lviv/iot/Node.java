5c2aaff1d0fac986dacd9ab6c0649a922454b33b
moroznastya <nastya.moroz2210@gmail.com>
2022-09-27 13:52:16 +0300

    deleted files from lab1

--- src/ua/lviv/iot/Node.java
+++
@@ -1,9 +0,0 @@
-package ua.lviv.iot;
-
-public class Node {
-    int data;
-    Node parent;
-    Node left;
-    Node right;
-    int color; // if node is black: color = 0; elif node is red: color = 1
-}
